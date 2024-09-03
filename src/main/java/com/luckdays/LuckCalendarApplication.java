e8413f7968f8d8de1486d019357c5b0f2b1f7da7
zyzz15620 <zyzz15620@gmail.com>
2024-09-03 13:08:03 +0700

    [ID-18] modified LuckController for Heroku

--- src/main/java/com/luckdays/LuckCalendarApplication.java
+++ src/main/java/com/luckdays/LuckCalendarApplication.java
@@ -2,12 +2,27 @@ package com.luckdays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
 @SpringBootApplication
-public class LuckCalendarApplication {
+public class LuckCalendarApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LuckCalendarApplication.class, args);
 	}
 
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		String port = System.getenv("PORT");
+		if (port != null) {
+			tomcat.setPort(Integer.parseInt(port));
+		}
+		return tomcat;
+	}
 }
