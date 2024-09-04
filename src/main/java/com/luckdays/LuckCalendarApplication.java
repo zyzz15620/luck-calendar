8213b4f84aa04a2fbb5ddcfdd97a9965d2c9ccd6
zyzz15620 <zyzz15620@gmail.com>
2024-09-05 05:22:48 +0700

    [ID-33] remove thymeleaf layout import

--- src/main/java/com/luckdays/LuckCalendarApplication.java
+++ src/main/java/com/luckdays/LuckCalendarApplication.java
@@ -1,11 +1,9 @@
 package com.luckdays;
 
-import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 
@@ -15,7 +13,6 @@ public class LuckCalendarApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(LuckCalendarApplication.class, args);
 	}
-
 
 	@Bean
 	public ServletWebServerFactory servletContainer() {
